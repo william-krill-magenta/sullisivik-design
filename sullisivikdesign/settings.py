af3dd282150a8b7f2772d403ba80a38f4536e498
William Heyman Krill <whk@magenta.dk>
2018-07-30 12:20:06 +0200

    Add polls app

--- sullisivikdesign/settings.py
+++ sullisivikdesign/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sullisivikdesign.apps.WebsiteConfig'
+    'polls.apps.PollsConfig',
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
