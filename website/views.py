8bd9961134f361a777d2fc60dbfc4212200e87f7
William Heyman Krill <whk@magenta.dk>
2018-08-06 11:35:06 +0200

    Add grid site

--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'website/testsite.html')
+
+def gridindex(request):
+    return render(request, 'website/gridsite.html')
