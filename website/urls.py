07089e31b3f624f5d82f8fa4143541517173f1bf
William Heyman Krill <whk@magenta.dk>
2018-09-04 10:20:46 +0200

    Separate demos from copypaste.html

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^grid$', views.gridindex),
+    url(r'^demo1$', views.demo1),
+    url(r'^demo2$', views.demo2),
+    url(r'^demo3$', views.demo3),
     url(r'^copypaste$', views.copypaste)
 ]
 
--- website/views.py
+++ website/views.py
@@ -6,3 +6,12 @@ def gridindex(request):
 
 def copypaste(request):
     return render(request, 'website/copypaste.html')
+
+def demo1(request):
+    return render(request, 'website/demo1.html')
+
+def demo2(request):
+    return render(request, 'website/demo2.html')
+
+def demo3(request):
+    return render(request, 'website/demo3.html')
